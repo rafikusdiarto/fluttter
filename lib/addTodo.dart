b5656951bc0fa1db4c3bf852e3099aa67f5d94ed
orymikoto <rosbergroll99@gmail.com>
2023-03-03 20:45:14 +0700

    add register page all

--- lib/addTodo.dart
+++ lib/addTodo.dart
@@ -13,32 +13,34 @@ class _AddTodoPageState extends State<AddTodoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color(0xff1d1e26),
-            Color(0xff252041),
-          ]),
-        ),
-        child:SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 30),
-              IconButton(onPressed: (){}, 
-              icon: Icon(
-                CupertinoIcons.arrow_left,
-                color: Colors.white,
-                size: 28,
-                ),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color(0xff1d1e26),
+              Color(0xff252041),
+            ]),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 30),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    CupertinoIcons.arrow_left,
+                    color: Colors.white,
+                    size: 28,
+                  ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
+                      const Text(
                         "Create",
                         style: TextStyle(
                           fontSize: 33,
@@ -47,8 +49,10 @@ class _AddTodoPageState extends State<AddTodoPage> {
                           letterSpacing: 4,
                         ),
                       ),
-                      SizedBox(height: 8,),
-                      Text(
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
                         "New Todo",
                         style: TextStyle(
                           fontSize: 33,
@@ -57,76 +61,76 @@ class _AddTodoPageState extends State<AddTodoPage> {
                           letterSpacing: 2,
                         ),
                       ),
-                        SizedBox(
-                          height: 25,
-                        ),
-                        label("Task Title"),
-                        SizedBox(height: 12,),
-                        title(context),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        label("Task Type"),
-                        SizedBox(
-                          height: 12,
-                        ),
-                            Row(
-                              children: [
-                                chipData("Urgent", 0xff2664fa),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                chipData("Santuy", 0xffffb703),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                chipData("Planned", 0xff2a9d8f),
-                              ],
-                            ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      label("Task Title"),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      title(context),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      label("Task Type"),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: [
+                          chipData("Urgent", 0xff2664fa),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          chipData("Santuy", 0xffffb703),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          chipData("Planned", 0xff2a9d8f),
+                        ],
+                      ),
                     ],
                   ),
-                  )
-            ],
-          ),
-        )
-      ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
 
-Widget chipData(String label, int color){
+Widget chipData(String label, int color) {
   return Chip(
     backgroundColor: Color(color),
     label: Text(
       label,
-      style: TextStyle(
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 15,
         fontWeight: FontWeight.w600,
       ),
     ),
-    labelPadding: EdgeInsets.symmetric(
+    labelPadding: const EdgeInsets.symmetric(
       horizontal: 17,
       vertical: 3.8,
     ),
-    );
+  );
 }
 
-
-Widget title(context){
+Widget title(context) {
   return Container(
     height: 55,
     width: MediaQuery.of(context).size.width,
     decoration: BoxDecoration(
-      color: Color(0xff2a2e3d),
+      color: const Color(0xff2a2e3d),
       borderRadius: BorderRadius.circular(15),
     ),
     child: TextFormField(
-      style: TextStyle(
+      style: const TextStyle(
         color: Colors.grey,
         fontSize: 17,
       ),
-      decoration: InputDecoration(
+      decoration: const InputDecoration(
         border: InputBorder.none,
         hintText: "Item To-do",
         hintStyle: TextStyle(
@@ -142,11 +146,10 @@ Widget title(context){
   );
 }
 
-
-Widget label(String label){
+Widget label(String label) {
   return Text(
     label,
-    style: TextStyle(
+    style: const TextStyle(
       color: Colors.white,
       fontWeight: FontWeight.w600,
       fontSize: 16.5,
---
+++ lib/feature/authentication/register.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/widget/input.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter/material.dart';
+// import 'package:login/home_page.dart';
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({super.key});
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[850],
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                      'lib/data/local/assets/images/bg.jpg')), // border: Border.all(color: Colors.white10),
+            ),
+            child: Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                // mainAxisSize: MainAxisSize.max,
+                children: [
+                  //Logo
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: const <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 50),
+                          child: Image(
+                            image: AssetImage(
+                                'lib/data/local/assets/images/fluttericon.png'),
+                            width: 75,
+                            height: 75,
+                          ),
+                        )
+                      ]),
+
+                  //Welcome Back
+                  Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Text(
+                        'Welcome Back',
+                        style: GoogleFonts.bebasNeue(
+                            fontSize: 33,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+
+                        // textAlign: TextAlign.start,
+                      )),
+                  Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.amber.withOpacity(0.3),
+                                spreadRadius: -3,
+                                blurRadius: 7,
+                                offset: const Offset(7, 7))
+                          ],
+                          color: Colors.grey[900],
+                          borderRadius: BorderRadius.circular(25)),
+                      margin: const EdgeInsets.only(left: 20, right: 20),
+                      padding: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: Column(
+                        children: [
+                          InputField(
+                              hint: true,
+                              hintText: "email",
+                              label: "email",
+                              obsecureText: false,
+                              controller: emailController),
+                          InputField(
+                              hint: false,
+                              hintText: "password",
+                              label: "password",
+                              obsecureText: true,
+                              controller: passwordController),
+                          Align(
+                              alignment: Alignment.centerRight,
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 10),
+                                child: Text(
+                                  "Forgot Password?",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(color: Colors.grey[200]),
+                                ),
+                              ))
+                        ],
+                      ))
+                ])));
+  }
+}
